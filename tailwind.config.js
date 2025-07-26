b3385212185579f8a24388e24f7bfb0d79c875a0
Lucas Arruda <1940532+larruda@users.noreply.github.com>
2025-07-26 20:56:14 -0300

    melhorias na aparencia

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_layouts/**/*.html',
+    './_includes/**/*.html',
+    './*.html',
+    './*.md',
+    './assets/**/*.js'
+  ],
+  darkMode: 'class', // Enable class-based dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

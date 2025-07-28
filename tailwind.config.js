66159f584f1c45536ae6751fb05d4ef86ed5c166
Lucas Arruda <1940532+larruda@users.noreply.github.com>
2025-07-28 11:45:38 -0300

    estilos de tipografia

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './_layouts/**/*.html',
-    './_includes/**/*.html',
-    './*.html',
-    './*.md',
-    './assets/**/*.js'
+    "./_layouts/**/*.html",
+    "./_includes/**/*.html",
+    "./*.html",
+    "./*.md",
+    "./assets/**/*.js",
   ],
-  darkMode: 'class', // Enable class-based dark mode
+  darkMode: "class", // Enable class-based dark mode
   theme: {
     extend: {},
-  },
-  plugins: [],
-}
+  }
+};
